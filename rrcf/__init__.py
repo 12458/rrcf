9e9d6383577a6fb55826bfb1a6783acb85530a1c
Shang En Sim <6071014+12458@users.noreply.github.com>
2025-10-30 23:42:54 -0400

    Update import statements and version retrieval to reflect module name change to "newrrcf"

--- rrcf/__init__.py
+++ rrcf/__init__.py
@@ -1,10 +1,10 @@
-from rrcf.rrcf import *
-from rrcf.shingle import shingle
+from newrrcf.newrrcf import *
+from newrrcf.shingle import shingle
 from importlib.metadata import version, PackageNotFoundError
 
 __version__: str
 
 try:
-    __version__ = version("rrcf")
+    __version__ = version("newrrcf")
 except PackageNotFoundError:
     __version__ = "unknown"
